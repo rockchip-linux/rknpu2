add257973de3391245a7d9ca0e6151536e4aa1f8
mxf13 <1805784360@qq.com>
2023-08-02 17:40:29 +0800

    delete some files used in android and armhf platform

--- examples/rknn_yolov5_demo/build-linux_RK3588.sh
+++ examples/rknn_yolov5_demo/build-linux_RK3588.sh
@@ -10,14 +10,14 @@ export CXX=${GCC_COMPILER}-g++
 ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
 
 # build
-BUILD_DIR=${ROOT_PWD}/build/build_linux_aarch64
+BUILD_DIR=${ROOT_PWD}/build/
 
 if [[ ! -d "${BUILD_DIR}" ]]; then
   mkdir -p ${BUILD_DIR}
 fi
 
 cd ${BUILD_DIR}
-cmake ../.. -DCMAKE_SYSTEM_NAME=Linux -DTARGET_SOC=${TARGET_SOC}
-make -j4
+cmake .. #/.. -DCMAKE_SYSTEM_NAME=Linux -DTARGET_SOC=${TARGET_SOC}
+make -j8
 make install
 cd -
--- examples/rknn_yolov5_demo/build/build_linux_aarch64/CMakeFiles/rknn_yolov5_demo.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for rknn_yolov5_demo.
-# This may be replaced when dependencies are built.
