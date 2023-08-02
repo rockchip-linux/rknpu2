a4709af013518de5029c9a9543ed725a7a7f6603
mxf13 <1805784360@qq.com>
2023-08-02 17:55:02 +0800

    add build files at my orange pi 5 for example

---
+++ examples/rknn_yolov5_demo/build/CMakeFiles/rknn_yolov5_demo.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pi/code/github/rknpu2/examples/rknn_yolov5_demo/src/main.cc" "CMakeFiles/rknn_yolov5_demo.dir/src/main.cc.o" "gcc" "CMakeFiles/rknn_yolov5_demo.dir/src/main.cc.o.d"
+  "/home/pi/code/github/rknpu2/examples/rknn_yolov5_demo/src/postprocess.cc" "CMakeFiles/rknn_yolov5_demo.dir/src/postprocess.cc.o" "gcc" "CMakeFiles/rknn_yolov5_demo.dir/src/postprocess.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ examples/rknn_yolov5_demo/build/CMakeFiles/rknn_yolov5_demo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rknn_yolov5_demo.dir/src/main.cc.o"
+  "CMakeFiles/rknn_yolov5_demo.dir/src/main.cc.o.d"
+  "CMakeFiles/rknn_yolov5_demo.dir/src/postprocess.cc.o"
+  "CMakeFiles/rknn_yolov5_demo.dir/src/postprocess.cc.o.d"
+  "rknn_yolov5_demo"
+  "rknn_yolov5_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rknn_yolov5_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
