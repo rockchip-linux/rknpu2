b5929b1c4e07515ea710db2eaaa561ead2529b1a
mxf13 <1805784360@qq.com>
2023-08-03 09:35:22 +0800

    modified CMakeLists.txt to change the install directory

--- examples/rknn_yolov5_demo/build/cmake_install.cmake
+++ examples/rknn_yolov5_demo/build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/pi/code/github/rknpu2/examples/rknn_yolov5_demo/install/rknn_yolov5_demo_Linux")
+  set(CMAKE_INSTALL_PREFIX "/home/pi/code/github/rknpu2/examples/rknn_yolov5_demo/install/")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
